95e06a3c391920f1a447768a814edfb440c92cec
longjes1 <83931035+longjes1@users.noreply.github.com>
2021-05-25 10:09:00 -0400

    Add files via upload

---
+++ Week 3 - Functions.R
@@ -0,0 +1,274 @@
+library(tidyverse)
+library(ggplot2)
+library(ggfortify)
+library(plyr)
+
+source("Week 2 - Fitting HMM.R")
+
+#Get Poisson marginal distribution 
+pois.marginal <- function(n, mod){
+  #Get stationary distribution
+  delta <-solve(t(diag(mod$m)-mod$gamma+1),rep(1,mod$m))
+  mpois <- delta[1]*dpois(x=0:n, lambda=mod$lambda[1])
+  for (i in 2:mod$m){
+    mpois <- mpois + delta[i]*dpois(x=0:n, lambda=mod$lambda[i])
+  }
+  return(data.frame(x=0:n, mpois=mpois))
+}
+
+#Computing log(forward probabilities) for Poisson distribution
+#See Section 4.1.1
+pois.HMM.lforward <- function(x, mod)
+  {
+    n <- length(x)
+    #Empty matrix of size mxn
+    lalpha <- matrix(NA, mod$m, n)
+    foo <- mod$delta*dpois(x[1], mod$lambda)
+    sumfoo <- sum(foo)
+    lscale <- log(sumfoo)
+    foo <- foo/sumfoo
+    #Set first column of the matrix lalpha
+    lalpha [ ,1] <- lscale + log(foo)
+    for (i in 2:n)
+      {
+        foo <- foo%*%mod$gamma*dpois(x[i], mod$lambda)
+        sumfoo <- sum(foo)
+        lscale <- lscale + log(sumfoo)
+        foo <- foo/sumfoo
+        #Set ith column of the matrix lalpha to alpha_i
+        lalpha[,i] <- log(foo) + lscale
+      }
+    return(lalpha)
+}
+
+#Computing log(backward probabilities) for Poisson distribution
+#See Section 4.1.2
+pois.HMM.lbackward <- function(x, mod)
+  {
+    n <- length(x)
+    m <- mod$m
+    lbeta <- matrix(NA,m,n)
+    #Set last column of matrix lbeta to 0
+    lbeta[,n] <- rep(0,m)
+    foo <- rep(1/m,m)
+    lscale <- log(m)
+    for (i in (n-1):1)
+      {
+        foo <- mod$gamma %*%(dpois (x[i+1] , mod$lambda)*foo)
+        #Set ith column of the matrix lbeta to beta_i
+        lbeta [,i] <- log(foo) + lscale
+        sumfoo <- sum(foo)
+        foo <- foo/sumfoo
+        lscale <- lscale + log(sumfoo)
+      }
+    return(lbeta)
+}
+
+#Conditional probabilities for Poisson distribution
+#Conditional probability that observation at time t equals
+#xc , given all observations other than that at time t.
+#Note: xc is a vector and the result (dxc) is a matrix .
+pois.HMM.conditional <- function(xc, x, mod)
+  {
+    n <- length(x)
+    m <- mod$m
+    nxc <- length(xc)
+    dxc <- matrix(NA, nrow=nxc, ncol=n)
+    Px <- matrix (NA, nrow=m, ncol=nxc)
+    #Set elements of Px to p_i(xc_j)
+    for (j in 1:nxc) Px[,j]<-dpois(xc[j], mod$lambda)
+    la <- pois.HMM.lforward(x, mod)
+    lb <- pois.HMM.lbackward(x, mod)
+    la <- cbind(log(mod$delta), la)
+    #Get maximum value in each column 
+    lafact <- apply(la, 2, max)
+    lbfact <- apply(lb, 2, max)
+    for (i in 1:n)
+      {
+        foo <- (exp(la[,i]-lafact[i])%*%mod$gamma)*exp(lb[,i]-lbfact[i])
+        foo <- foo/sum(foo)
+        dxc[,i] <- foo%*%Px
+      }
+    return(dxc)
+}
+
+#Conditional probability that observation at time t equals
+#xc, given observations at time 1 to t-1. 
+#Note: xc is a vector and the result (dxc) is a matrix .
+pois.HMM.conditional2 <- function(xc, x, mod)
+{
+  n <- length(x)
+  m <- mod$m
+  nxc <- length(xc)
+  dxc <- matrix(NA, nrow=nxc, ncol=n)
+  Px <- matrix (NA, nrow=m, ncol=nxc)
+  for (j in 1:nxc) Px[,j]<-dpois(xc[j], mod$lambda)
+  la <- pois.HMM.lforward(x, mod)
+  la <- cbind(log(mod$delta), la)
+  lafact <- apply(la, 2, max)
+  for (i in 2:n)
+  {
+    foo <- exp(la[,i]-lafact[i])
+    foo <- foo/sum(foo)
+    dxc[,i] <- foo%*%mod$gamma%*%Px
+  }
+  return(dxc)
+}
+
+#Normal pseudo-residuals for Poisson HMM
+#Type can be "ordinary" or "forecast"
+#Output is lower, upper and middle pseudo-residuals
+pois.HMM.pseudo_residuals <- function(x, mod, type)
+{
+  n <- length(x)
+  if (type=="ordinary"){
+    cdists <- pois.HMM.conditional(xc=0:max(x), x, mod)
+  }
+  else if (type=="forecast"){
+    cdists <- pois.HMM.conditional2(xc=0:max(x), x, mod)
+  }
+  #Each column of cumdists starts with 0, remaining elements i are cumulative sum of 
+  #elements in the column of cdists up to that cell 
+  cumdists <- rbind(rep(0,n), apply(cdists, 2, cumsum))
+  ulo <- uhi <- rep(NA, n)
+  for (i in 1:n)
+  {
+    ulo[i] <- cumdists[x[i]+1 ,i]
+    uhi[i] <- cumdists[x[i]+2 ,i]
+  }
+  umi <- 0.5*(ulo+uhi)
+  npsr <- qnorm(rbind(ulo, umi, uhi))
+  return(data.frame(lo=npsr[1,], mi=npsr[2,], hi=npsr[3,]))
+}
+
+#Get normal marginal distribution 
+norm.marginal <- function(start, end, n, mod){
+  #Get stationary distribution
+  delta <-solve(t(diag(mod$m)-mod$gamma+1),rep(1,mod$m))
+  x <- seq(start, end, length.out = n)
+  mnorm <- delta[1]*dnorm(x, mean=mod$mu[1], sd=mod$sigma[1])
+  for (i in 2:mod$m){
+    mnorm <- mnorm + delta[i]*dnorm(x, mean=mod$mu[i], sd=mod$sigma[i])
+  }
+  return(data.frame(x=x, mnorm=mnorm))
+}
+
+#Computing log(forward probabilities) for normal distribution
+norm.HMM.lforward <- function(x, mod)
+{
+  n <- length(x)
+  lalpha <- matrix(NA, mod$m, n)
+  foo <- mod$delta*dnorm(x[1], mod$mu, mod$sigma)
+  sumfoo <- sum(foo)
+  lscale <- log(sumfoo)
+  foo <- foo/sumfoo
+  lalpha [ ,1] <- lscale + log(foo)
+  for (i in 2:n)
+  {
+    foo <- foo%*%mod$gamma*dnorm(x[i], mod$mu, mod$sigma)
+    sumfoo <- sum(foo)
+    lscale <- lscale + log(sumfoo)
+    foo <- foo/sumfoo
+    lalpha[,i] <- log(foo) + lscale
+  }
+  return(lalpha)
+}
+
+#Computing log(backward probabilities) for normal distribution
+norm.HMM.lbackward <- function(x, mod)
+{
+  n <- length(x)
+  m <- mod$m
+  lbeta <- matrix(NA,m,n)
+  lbeta[,n] <- rep(0,m)
+  foo <- rep(1/m,m)
+  lscale <- log(m)
+  for (i in (n-1):1)
+  {
+    foo <- mod$gamma%*%(dnorm(x[i+1], mod$mu, mod$sigma)*foo)
+    lbeta [,i] <- log(foo) + lscale
+    sumfoo <- sum(foo)
+    foo <- foo/sumfoo
+    lscale <- lscale + log(sumfoo)
+  }
+  return(lbeta)
+}
+
+#Conditional probabilities for normal distribution
+#Conditional probability that observation at time t equals
+#xc , given all observations other than that at time t.
+#Note: xc is a vector and the result (dxc) is a matrix .
+norm.HMM.conditional <- function(xc, x, mod)
+{
+  n <- length(x)
+  m <- mod$m
+  nxc <- length(xc)
+  dxc <- matrix(NA, nrow=nxc, ncol=n)
+  Px <- matrix (NA, nrow=m, ncol=nxc)
+  for (j in 1:nxc) Px[,j]<-dnorm(xc[j], mod$mu, mod$sigma)
+  la <- norm.HMM.lforward(x, mod)
+  lb <- norm.HMM.lbackward(x, mod)
+  la <- cbind(log(mod$delta), la)
+  lafact <- apply(la, 2, max)
+  lbfact <- apply(lb, 2, max)
+  for (i in 1:n)
+  {
+    foo <- (exp(la[,i]-lafact[i])%*%mod$gamma)*exp(lb[,i]-lbfact[i])
+    foo <- foo/sum(foo)
+    dxc[,i] <- foo%*%Px
+  }
+  return(dxc)
+}
+
+#Conditional probability that observation at time t equals
+#xc, given observations at time 1 to t-1. 
+#Note: xc is a vector and the result (dxc) is a matrix .
+norm.HMM.conditional2 <- function(xc, x, mod)
+{
+  n <- length(x)
+  m <- mod$m
+  nxc <- length(xc)
+  dxc <- matrix(NA, nrow=nxc, ncol=n)
+  Px <- matrix (NA, nrow=m, ncol=nxc)
+  for (j in 1:nxc) Px[,j]<-dnorm(xc[j], mod$mu, mod$sigma)
+  la <- norm.HMM.lforward(x, mod)
+  la <- cbind(log(mod$delta), la)
+  lafact <- apply(la, 2, max)
+  for (i in 2:n)
+  {
+    foo <- exp(la[,i]-lafact[i])
+    foo <- foo/sum(foo)
+    dxc[,i] <- foo%*%mod$gamma%*%Px
+  }
+  return(dxc)
+}
+
+#Normal pseudo-residuals for Normal HMM
+#Type can be "ordinary" or "forecast"
+norm.HMM.pseudo_residuals <- function(x, mod, type)
+{
+  n <- length(x)
+  if (type=="ordinary"){
+    cdists <- norm.HMM.conditional(xc=x, x, mod)
+  }
+  else if (type=="forecast"){
+    cdists <- norm.HMM.conditional2(xc=x, x, mod)
+  }
+  for (i in 1:n)
+  {
+    xord <- sort(x)
+    cdf <- rep(NA, n)
+    cdford <- rep(NA, n)
+    cdford[1] <- 0
+    for (j in 2:n)
+    {
+      index <- which(x==xord[j]) 
+      cdford[j] <- cdford[j-1] + cdists[index,i]*(xord[j]-xord[j-1])
+      cdf[index] <- cdford[j]
+    }
+    index <- which(x==xord[1]) 
+    cdf[index] <- 1-cdford[n]
+  }
+  npsr <- qnorm(cdf)
+  return(data.frame(npsr=npsr, x=x))
+}
---
+++ Week 3 - Testing with Simulated Data.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(ggplot2)
+library(ggfortify)
+library(plyr)
+library(bayesforecast)
+library(gridExtra)
+
+source("Week 2 - Fitting HMM.R")
+source("Week 3 - Functions.R")
+
+#Testing functions for Poisson distributions
+m <- 3
+lambda <- c(2, 5, 8)
+gamma <- matrix(c(0.1, 0.2, 0.7,
+                  0.3, 0.4, 0.3,
+                  0.6, 0.3, 0.1), nrow = m, byrow = TRUE)
+delta <- c(0.4, 0.2, 0.4)
+mod <- list(m=m, lambda=lambda, gamma=gamma, delta=delta)
+#Generate sample
+pois.sample <- pois.HMM.generate_sample(1000, mod)
+head(pois.sample)
+#Plots of sample
+graph_hmm_output(pois.sample)
+graph_hmm_hist(pois.sample)
+ggacf(pois.sample$obs)+
+  theme_minimal()
+#Get MLE
+lambda0 <- c(1, 4, 7)
+gamma0 <- matrix(rep(1/m, m*m), nrow = m, byrow = TRUE)
+delta0 <- rep(1/m, m)
+pois.mle <- pois.HMM.mle(pois.sample$obs, m, lambda0, gamma0, delta0, stationary=TRUE)
+pois.mle
+#Decode states
+pois.decoding <- pois.HMM.viterbi(pois.sample$obs, mod)
+count(pois.decoding$state-pois.sample$state)
+#Get marginal distribution
+nx <- max(pois.sample$obs)
+mpois <- pois.marginal(nx, pois.mle)
+ggplot()+
+  geom_histogram(data=pois.sample, aes(x=obs, y=..density..), binwidth=1, colour="navy", fill="light blue")+
+  geom_line(data=mpois, aes(x=x,y=mpois), colour="red", size=1)+
+  theme_minimal()
+#Get pseudo-residuals
+pois.pr <- pois.HMM.pseudo_residuals(pois.sample$obs, pois.mle, "forecast") 
+pois.pr <- pois.pr %>% filter(lo != -Inf)
+view(pois.opr)
+#Index plot of pseudo-residuals
+ggplot(pois.pr)+
+  geom_point(aes(x=c(1:length(lo)), y=lo), size=0.5, colour="black")+
+  geom_point(aes(x=c(1:length(hi)), y=hi), size=0.5, colour="red")+
+  theme_minimal()
+#Histograms of pseudo-residuals
+histlo <- ggplot(pois.pr, aes(lo))+
+  geom_histogram(aes(y=..density..),  colour="navy", fill="light blue")+
+  stat_function(fun=dnorm, colour="red")+
+  theme_minimal()
+histmi <- ggplot(pois.pr, aes(mi))+
+  geom_histogram(aes(y=..density..), colour="navy", fill="light blue")+
+  stat_function(fun=dnorm, colour="red")+
+  theme_minimal()
+histhi <- ggplot(pois.pr, aes(hi))+
+  geom_histogram(aes(y=..density..), colour="navy", fill="light blue")+
+  stat_function(fun=dnorm, colour="red")+
+  theme_minimal()
+grid.arrange(histlo, histmi, histhi, ncol=3)
+#QQ plot of pseudo-residuals
+ggplot(pois.pr, aes(sample=mi))+
+  stat_qq()+
+  stat_qq_line()+
+  theme_minimal()
+#ACF of pseudo-residuals
+acflo <- ggacf(pois.pr$lo)+
+  theme_minimal()
+acfmi <- ggacf(pois.pr$mi)+
+  theme_minimal()
+acfhi <- ggacf(pois.pr$hi)+
+  theme_minimal()
+grid.arrange(acflo, acfmi, acfhi, ncol=3)
+
+#Testing functions for normal distributions
+m <- 3
+mu <- c(2, 5, 8)
+sigma <- c(2, 4, 6)
+gamma <- matrix(c(0.1, 0.2, 0.7,
+                  0.3, 0.4, 0.3,
+                  0.6, 0.3, 0.1), nrow = m, byrow = TRUE)
+delta <- c(0.4, 0.2, 0.4)
+mod2 <- list(m=m, mu=mu, sigma=sigma, gamma=gamma, delta=delta)
+#Generate sample
+norm.sample <- norm.HMM.generate_sample(1000, mod2)
+head(norm.sample)
+#Plots of sample
+graph_hmm_output(norm.sample)
+graph_hmm_hist(norm.sample)
+ggacf(norm.sample$obs)+
+  theme_minimal()
+#Get MLE
+mu0 <- c(5, 5, 5)
+sigma0 <- c(5, 5, 5)
+gamma0 <- matrix(rep(1/m, m*m), nrow = m, byrow = TRUE)
+delta0 <- rep(1/m, m)
+norm.mle <- norm.HMM.mle(norm.sample$obs, m, mu0, sigma0, gamma0, delta0, stationary=TRUE)
+norm.mle
+#Decode states
+norm.decoding <- norm.HMM.viterbi(norm.sample$obs, mod2)
+count(norm.decoding-norm.sample$state)
+#Get marginal distribution
+start <- min(norm.sample$obs)
+end <- max(norm.sample$obs)
+mnorm <- norm.marginal(start, end, 1000, norm.mle)
+ggplot()+
+  geom_histogram(data=norm.sample, aes(x=obs, y=..density..), binwidth=1, colour="navy", fill="light blue")+
+  geom_line(data=mnorm, aes(x=x,y=mnorm), colour="red", size=1)+
+  theme_minimal()
+#Get pseudo-residuals
+norm.pr <- norm.HMM.pseudo_residuals(norm.sample$obs, norm.mle, "forecast") 
+#Index plot of pseudo-residuals
+ggplot(norm.pr)+
+  geom_point(aes(x=c(1:length(npsr)), y=npsr), size=0.5, colour="black")+
+  theme_minimal()
+#Histogram of pseudo-residuals
+ggplot(norm.pr, aes(npsr))+
+  geom_histogram(aes(y=..density..),  colour="navy", fill="light blue")+
+  stat_function(fun=dnorm, colour="red")+
+  theme_minimal()
+#QQ plot of pseudo-residuals
+ggplot(norm.pr, aes(sample=npsr))+
+  stat_qq()+
+  stat_qq_line()+
+  theme_minimal()
+#ACF of pseudo-residuals
+ggacf(norm.pr)+
+  theme_minimal()
+
