9719aeed1f043564486183ecdd27a8db1d039be2
longjes1 <83931035+longjes1@users.noreply.github.com>
2021-06-07 10:47:24 -0400

    Update Week 4 - Standard Errors and Confidence Intervals.R

--- Week 4 - Standard Errors and Confidence Intervals.R
+++ Week 4 - Standard Errors and Confidence Intervals.R
@@ -3,8 +3,69 @@ library(ggplot2)
 library(ggfortify)
 library(plyr)
 
+setwd("C:/Jessica/UofT Y4/Research/Coding")
+
 source("Week 2 - Fitting HMM.R")
 source("Week 3 - Functions.R")
+
+#Using Hessian
+norm.HMM.mle <- function(x, m, mu0, sigma0, gamma0, delta0=NULL, stationary=TRUE,...)
+{
+  parvect0 <- norm.HMM.pn2pw(m, mu, sigma0, gamma0, delta0, stationary=stationary)
+  mod <- nlm(norm.HMM.mllk, parvect0, x=x,m=m, stationary=stationary, hessian=TRUE)
+  pn <- norm.HMM.pw2pn(m=m, mod$estimate, stationary=stationary)
+  mllk <- mod$minimum
+  if (det(mod$hessian) != 0){
+    h <- solve(mod$hessian)
+    jacobian <- norm.jacobian(m, mod$estimate, stationary=stationary) 
+    h <- t(jacobian)%*%h%*%jacobian
+  }
+  np <- length(parvect0)
+  AIC <- 2*(mllk+np)
+  n <- sum(!is.na(x))
+  BIC <- 2*mllk+np*log(n)
+  if (det(mod$hessian) != 0){
+    list(m=m, mu=pn$mu, sigma=pn$sigma, gamma=pn$gamma, delta=pn$delta,
+         code=mod$code, mllk=mllk, AIC=AIC, BIC=BIC, invhessian=h)
+  }
+  else{
+    list(m=m, mu=pn$mu, sigma=pn$sigma, gamma=pn$gamma, delta=pn$delta,
+         code=mod$code, mllk=mllk, AIC=AIC, BIC=BIC)
+  }
+}
+
+norm.jacobian <- function(m, parvect, stationary=TRUE){
+  jacobian <- matrix(0, nrow=length(parvect), ncol=length(parvect))
+  jacobian[1:m, 1:m] <- diag(m)
+  jacobian[(m+1):(2*m), (m+1):(2*m)] <- diag(exp(parvect[(m+1):(2*m)]))
+  gamma0 <- diag(m)
+  gamma0[!gamma0] <- exp(parvect[(2*m+1):(m+m*m)])
+  count <- 0
+  for (i in 1:m){
+    for (j in 1:m){
+      if (j != i){
+        count <- count + 1
+        k <- (sum(gamma0[i,]))
+        foo <- -(gamma0[i,]*gamma0[i,j])/(k^2)
+        foo[j] <- (gamma0[i,j]*k - gamma0[i,j]^2)/(k^2)
+        foo <- foo[-i]
+        jacobian[2*m+count, (2*m+(i-1)*(m-1)+1):(2*m+i*(m-1))] <- foo
+      }
+    }
+  }
+  if(!stationary)
+  {
+    delta0 <- c(1, exp(parvect[(m+m*m+1):(m*m+2*m-1)]))
+    k <- (sum(gamma0[i,]))
+    for (i in 2:m){
+      foo <- -(delta0*delta0[i])/(k^2)
+      foo[i] <- (delta0[i]*k - delta0[i]^2)/(k^2)
+      foo <- foo[-1]
+      jacobian[(m+1)*m, ((m+1)*m):((m+2)*m-1)]<- foo
+    }
+  }
+  return(jacobian)
+}
 
 #Using bootstrapping
 norm.bootstrap.estimates <- function(mod, n, stationary){
@@ -85,14 +146,19 @@ mu0 <- c(5, 5, 5)
 sigma0 <- c(5, 5, 5)
 gamma0 <- matrix(rep(1/m, m*m), nrow = m, byrow = TRUE)
 delta0 <- rep(1/m, m)
-norm.mle <- norm.HMM.mle(norm.sample$obs, m, mu0, sigma0, gamma0, delta0, stationary=FALSE)
+norm.mle <- norm.HMM.mle(norm.sample$obs, m, mu0, sigma0, gamma0, delta0, stationary=TRUE)
 norm.mle
+sd <- sqrt(diag(norm.mle$invhessian))
+theta <- c(norm.mle$mu, norm.mle$sigma, 
+           norm.mle$gamma[1,2:3],
+           norm.mle$gamma[2,-2],
+           norm.mle$gamma[3, 1:2])
+norm.cih <- list(lower=theta-1.96*sd, upper=theta+1.96*sd)
+norm.cih
 #Testing bootstrapping 
-n <- 10
-norm.bootstrap <- norm.bootstrap.estimates(norm.mle, n, FALSE)
-norm.bootstrap$mu
+n <- 100
+norm.bootstrap <- norm.bootstrap.estimates(norm.mle, n, TRUE)
 norm.cov <- norm.bootstrap.covariance(norm.bootstrap, m, n)
 norm.cov
-norm.ci <- norm.bootstrap.ci(mod, norm.bootstrap, 0.05, m, n)
-norm.ci
-
+norm.cib <- norm.bootstrap.ci(norm.mle, norm.bootstrap, 0.05, m, n)
+norm.cib
