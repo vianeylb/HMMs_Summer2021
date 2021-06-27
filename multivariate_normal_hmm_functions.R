1fe7d588a0bac4fee10d97cc82d4609c80cb2e1d
longjes1 <83931035+longjes1@users.noreply.github.com>
2021-06-27 14:41:12 -0400

    Update multivariate_normal_hmm_functions.R

--- multivariate_normal_hmm_functions.R
+++ multivariate_normal_hmm_functions.R
@@ -218,7 +218,7 @@ mvnorm_hmm_viterbi <- function(x, mod) {
   for (t in (n - 1):1) {
     iv[t] <- which.max(mod$gamma[, iv[t + 1]] * xi[t, ])
   }
-  return(data_frame(index = 1:n, states = iv))
+  return(data_frame(index = 1:n, state = iv))
 }
 
 # Computing log(forward probabilities) for normal distribution
