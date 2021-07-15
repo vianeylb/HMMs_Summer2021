bea50c871d5ad280013e6f1ccc33c777a18e0954
longjes1 <83931035+longjes1@users.noreply.github.com>
2021-07-14 20:23:07 -0400

    Update exploring_lab_data_dynamic.R

--- exploring_lab_data_dynamic.R
+++ exploring_lab_data_dynamic.R
@@ -9,10 +9,6 @@ library(lemon)
 library(GGally)
 library(RColorBrewer)
 
-setwd("C:/Jessica/UofT Y4/Research/Coding")
-
-source("univariate_normal_hmm_functions.R")
-
 setwd("C:/Jessica/UofT Y4/Research/Coding/Lab Data")
 
 get_custom_data <- function(data_file, timestamps_file, filename) {
@@ -36,22 +32,34 @@ get_custom_data <- function(data_file, timestamps_file, filename) {
     colnames(timestamps)[which(names(timestamps) ==
                                  "Time.End")] <- "Time.Stop"
   }
-
+  
+  # Filter out rows with NA for data values
+  data <- data %>% drop_na()
+  
   # Add Behavior and Prey column to main data set, based on data in timestamps
   data$Time <- chron(times = data$Time)
   timestamps$Time.Start <- chron(times = timestamps$Time.Start)
   timestamps$Time.Stop <- chron(times = timestamps$Time.Stop)
-  data$Behavior <- ifelse(timestamps$Time.Start <= data$Time &
-                          timestamps$Time.Stop >= data$Time,
-                          timestamps$Behavior, NA
-  )
-  data$Prey <- ifelse(timestamps$Time.Start <= data$Time &
-                      timestamps$Time.Stop >= data$Time,
-                      timestamps$Prey, NA
-  )
+  data$Behavior <- NA
+  data$Prey <- NA
+  
+  for(i in 1:nrow(timestamps)) {
+    time_start <- timestamps[i,]$Time.Start
+    time_stop <- timestamps[i,]$Time.Stop
+    data$Behavior <- ifelse(time_start <= data$Time &
+                              time_stop >= data$Time,
+                            timestamps[i,]$Behavior, data$Behavior
+    )
+    data$Prey <- ifelse(time_start <= data$Time &
+                          time_stop >= data$Time,
+                        timestamps[i,]$Prey, data$Prey
+    )
+  }
+  
   # Write to CSV
   write.csv(data, file = filename, row.names = FALSE)
 }
+
 
 line_plot <- function(data, filename) {
   plotx <- ggplot(data, aes(x = Time, y = X_dynamic)) +
@@ -136,7 +144,7 @@ pacf_plot <- function(data, filename) {
 
 behavior_plot <- function(data, filename) {
   plotx <- ggplot(data, aes(x = Time, y = X_dynamic, colour = Behavior)) +
-    geom_point(size = 0.5) +
+    geom_line() +
     theme_minimal() +
     theme(
       axis.text.x = element_blank(),
@@ -145,7 +153,7 @@ behavior_plot <- function(data, filename) {
     ) +
     scale_color_brewer(palette = "Set1")
   ploty <- ggplot(data, aes(x = Time, y = Y_dynamic, colour = Behavior)) +
-    geom_point(size = 0.5) +
+    geom_line() +
     theme_minimal() +
     theme(
       axis.text.x = element_blank(),
@@ -154,7 +162,7 @@ behavior_plot <- function(data, filename) {
     ) +
     scale_color_brewer(palette = "Set1")
   plotz <- ggplot(data, aes(x = Time, y = Z_dynamic, colour = Behavior)) +
-    geom_point(size = 0.5) +
+    geom_line() +
     theme_minimal() +
     theme(
       axis.text.x = element_blank(),
@@ -167,7 +175,7 @@ behavior_plot <- function(data, filename) {
                                      position = "right")
   ggsave(paste(filename, "dynamic.png", sep = "_"), plot)
   plot_odba <- ggplot(data, aes(x = Time, y = ODBA, colour = Behavior)) +
-    geom_point(size = 0.5) +
+    geom_line() +
     theme_minimal() +
     theme(
       axis.text.x = element_blank(),
@@ -306,105 +314,54 @@ pairs_plot <- function(data, filename) {
   ggsave(filename, plot)
 }
 
-pseudo_residual_plot <- function(data, filename) {
-  # Index plot of pseudo-residuals
-  plot_index <- ggplot(data) +
-    geom_point(aes(x = index, y = npsr), size = 0.5, colour = "black") +
-    theme_minimal()
-  # Histogram of pseudo-residuals
-  plot_hist <- ggplot(data, aes(npsr)) +
-    geom_histogram(aes(y = ..density..), colour = "navy", fill = "light blue") +
-    stat_function(fun = dnorm, colour = "red") +
-    theme_minimal()
-  # QQ plot of pseudo-residuals
-  plot_qq <- ggplot(data, aes(sample = npsr)) +
-    stat_qq() +
-    stat_qq_line() +
-    theme_minimal()
-  # ACF of pseudo-residuals
-  plot_acf <- ggacf(data$npsr) +
-    theme_minimal()
-  plot <- grid.arrange(plot_index, plot_hist, plot_qq, plot_acf,
-                       nrow = 2, ncol = 2)
-  ggsave(paste(filename, "png", sep = "."), plot)
+behavior_pairs_plot_dynamic <- function(data, filename) {
+  behaviors <- unique(data$Behavior)
+  for (i in seq_len(length(behaviors))) {
+    behavior <- behaviors[i]
+    subdata <- data %>% filter(Behavior == behavior)
+    dynamic_data <- subdata %>% select(X_dynamic, Y_dynamic, Z_dynamic)
+    
+    plot <- ggpairs(dynamic_data,
+                    lower = list(continuous = wrap("smooth", size = 0.1)),
+                    diag = list(continuous = "bar")
+    ) +
+      theme_minimal()
+    
+    ggsave(paste(filename, behavior, "correlation.png", sep = "_"), plot)
+  }
 }
 
-data_file <- "Lady_27Mar17_Dynamic25Hz.csv"
-timestamps_file <- "TimestampedData_Lady_27Mar17.csv"
-filename <- "Custom_Lady_27Mar17_dynamic.csv"
+get_plots_dynamic <- function(names){
+  n <- length(names)
+  for (i in 1:n){
+    name <- names[i]
+    filename <- paste("Custom", name, "dynamic.csv", sep = "_")
+    data <- read.csv(filename)
+    labelled_data <- data %>% filter(!is.na(Behavior))
+    dynamic_data <- data %>% select(X_dynamic, Y_dynamic, Z_dynamic)
+    
+    line_plot(data, paste(name, "line_plot", sep = "_"))
+    hist_plot(data, paste(name, "histogram", sep = "_"))
+    acf_plot(data, paste(name, "acf", sep = "_"))
+    pacf_plot(data, paste(name, "pacf", sep = "_"))
+    behavior_plot(data, paste(name, "plot_behavior", sep = "_"))
+    behavior_plot(labelled_data, paste(name, "plot_behavior_filtered", sep = "_"))
+    filtered_hist(labelled_data, paste(name, "histogram_filtered", sep = "_"))
+    behavior_hist(labelled_data, paste(name, "histogram_behavior", sep = "_"))
+    pairs_plot(dynamic_data, paste(name, "correlation_dynamic.png", sep = "_"))
+    behavior_pairs_plot_dynamic(data, paste(name, "dynamic", sep = "_"))
+  }
+}
+
+data_file <- "BigGuy_15Feb18_Dynamic25Hz.csv"
+timestamps_file <- "TimestampedData_BigGuy_15Feb18.csv"
+filename <- "Custom_BigGuy_15Feb18_dynamic.csv"
 get_custom_data(data_file, timestamps_file, filename)
-data <- read.csv(filename)
-labelled_data <- data %>% filter(!is.na(Behavior))
-dynamic_data <- data %>% select(X_dynamic, Y_dynamic, Z_dynamic)
 
-line_plot(data, "Lady_27Mar17_line_plot")
-hist_plot(data, "Lady_27Mar17_histogram") 
-acf_plot(data, "Lady_27Mar17_acf")
-pacf_plot(data, "Lady_27Mar17_pacf")
-behavior_plot(data, "Lady_27Mar17_plot_behavior")
-behavior_plot(labelled_data, "Lady_27Mar17_plot_behavior_filtered")
-filtered_hist(labelled_data, "Lady_27Mar17_histogram_filtered")
-behavior_hist(labelled_data, "Lady_27Mar17_histogram_behavior")
-pairs_plot(dynamic_data, "Lady_27Mar17_correlation_dynamic.png")
-
-
-ggplot(filtered_data, aes(x = Time, y = X_dynamic, colour = Behavior)) +
-  geom_line() +
-  theme_minimal() +
-  theme(
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    text = element_text(size = 7)
-  ) +
-  scale_color_brewer(palette = "Set1")
-
-n <- length(data$Behavior)
-indicies <- c(1, which(data$Behavior != lag(data$Behavior)), n)
-head(indicies)
-max(indicies)
-filtered_data <- data %>% slice(450000:500000)
-head(filtered_data)
-
-# Loading data (from other laptop)
-load("norm_mle4X.RData")
-load("norm_mle4Y.RData")
-load("norm_mle4Z.RData")
-load("norm_pr4X.RData")
-load("norm_pr4Y.RData")
-load("norm_pr4Z.RData")
-
-# Pseudo-residual plots
-pseudo_residual_plot(norm_pr4X, "BigDaddy_3Apr17_pseudo_residuals_X")
-pseudo_residual_plot(norm_pr4Y, "BigDaddy_3Apr17_pseudo_residuals_Y")
-pseudo_residual_plot(norm_pr4Z, "BigDaddy_3Apr17_pseudo_residuals_Z")
-
-# 4 states: Feeding, Non-direction motion, Resting, and Swimming
-mu4 <- c(0, 0, 0, 0)
-sigma4 <- c(0.05, 0.1, 0.01, 0.2)
-gamma4 <- gamma4 <- matrix(c(
-  0.7, 0.1, 0.1, 0.1,
-  0.1, 0.7, 0.1, 0.1,
-  0.1, 0.1, 0.7, 0.1,
-  0.1, 0.1, 0.1, 0.7
-), nrow = 4, byrow = TRUE)
-delta4 <- c(0.1, 0.1, 0.7, 0.1)
-norm_mle4X <- norm_hmm_mle(filtered_data$X_dynamic,
-                           4, mu4, sigma4, gamma4, delta4,
-                           stationary = TRUE)
-norm_mle4X
-norm_decoding4X <- norm_hmm_viterbi(filtered_data$X_dynamic, norm_mle4X)
-count(norm_decoding4X, "states")
-# Marginal distribution and histogram
-start <- min(filtered_data$X_dynamic)
-end <- max(filtered_data$X_dynamic)
-mnorm4X <- norm_marginal(start, end, 50000, norm_mle4X)
-ggplot() +
-  geom_histogram(data = filtered_data,
-                 aes(x = X_dynamic, y = ..density..),
-                 binwidth = 1, colour = "navy", fill = "light blue") +
-  geom_line(data = mnorm4X, aes(x = x, y = mnorm), colour = "red", size = 1) +
-  theme_minimal()
-# Get pseudo-residuals
-norm_pr4X <- norm_hmm_pseudo_residuals(filtered_data$X_dynamic,
-                                       norm_mle4X, "forecast",
-                                       stationary = TRUE)
+names <- c("BigDaddy_3Apr17",
+           "BigDaddy_20Mar17",
+           "BigGuy_15Feb18",
+           "Eliza_7Sept17",
+           "Eliza_20Sept17",
+           "Lady_27Mar17")
+get_plots_dynamic(names)
